23bcce73f6cf8eb59418039b8007542f9798d1aa
bruuliyant <146166750+bruuliyant@users.noreply.github.com>
2024-05-08 12:47:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ let form = doc.querySelector('.forma')
 
 forma.addEventListener("submit", (event) => {
     event.preventDefault()
+    let time = new Date()
     let p_text = doc.createElement('h1')
     let p = doc.createElement('p')
     let d = doc.createElement('img')
+    let a = doc.createElement('input')
+    a.type = 'date'
     p_text.textContent = forma[0].value
-    p.textContent = '18:44'
+    p.textContent = time.toLocaleTimeString()
     d.src = './Group 14.svg'
     p.classList.add('p')
     d.classList.add('d')
+    a.classList.add('sa')
     forma.appendChild(p_text)
     forma.appendChild(p)
     forma.appendChild(d)
+    forma.appendChild(a)
     forma[0].value = ""
     console.log(FormData);
     d.addEventListener("click", (event) => {
@@ -27,6 +32,6 @@ forma.addEventListener("submit", (event) => {
         p_text.classList.toggle('p_text_netu')
         p.classList.toggle('d_netu')
         d.classList.toggle('p_netu')
+        a.classList.toggle('a_netu')
     })
 })
-
